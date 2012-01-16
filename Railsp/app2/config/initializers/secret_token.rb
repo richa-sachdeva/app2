c29284ba90adeeb59863c6da47e27e47fc6b2454
richa-sachdeva <sach.richa@gmail.com>
2012-01-16 17:15:52 +0530

    first commit

---
+++ Railsp/app2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = '52f747a986c822935b26ecdd7140662dd4f8e0c924698473ac8bb506e5a77421202df0241fe739c654e8fc34cbde3d5a7f0331a479e966a4c6f17ab372e7ea1d'
---
+++ Railsp/first_hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstHello::Application.config.secret_token = '1db302ae79ec98c65c11edbace4a50fdf5005414ffaa7ff40fab0df2a08c8535ae72191aa306faffdb837746f2995bc336c700e106bdd0fda8b1fc7a7b2c7bee'
